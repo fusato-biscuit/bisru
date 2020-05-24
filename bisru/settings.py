b67a942608fd8711d76388f7430ff50c63fc7463
fusato_yuuichirou <aquariusstars.1-29@docomo.ne.jp>
2020-05-24 08:20:06 -0400

    whitenoiseミドルウェアをsecurityミドルウェアの次に持ってきた

--- bisru/settings.py
+++ bisru/settings.py
@@ -50,13 +50,13 @@ AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'bisru.urls'
