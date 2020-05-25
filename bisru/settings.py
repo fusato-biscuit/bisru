c247dd0462ac57ca92783dcb3645cd3aa45722e9
fusato_yuuichirou <aquariusstars.1-29@docomo.ne.jp>
2020-05-25 07:36:56 -0400

    直した

--- bisru/settings.py
+++ bisru/settings.py
@@ -139,10 +139,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 try:
     from.local_settings import *
