a87bcdd25648ab6c20d627c932a91713ab3b3993
fusato_yuuichirou <aquariusstars.1-29@docomo.ne.jp>
2020-05-25 03:25:27 -0400

    mediaつまり画像アップロードに関する設定を消した。

--- bisru/settings.py
+++ bisru/settings.py
@@ -141,8 +141,8 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
 
 try:
     from.local_settings import *
